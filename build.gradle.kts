7b67a66901a9fede6a5161675cfd424423b908f2
Robin Blondin <robin.blondin@protonmail.com>
2025-08-12 22:29:10 +0200

    Added mapstruct dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
       implementation("org.springframework.boot:spring-boot-starter-security")
       implementation("org.springframework.boot:spring-boot-starter-web")
       implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+      implementation("org.mapstruct:mapstruct:1.6.3")
       implementation("org.jetbrains.kotlin:kotlin-reflect")
       developmentOnly("org.springframework.boot:spring-boot-devtools")
       runtimeOnly("org.postgresql:postgresql")
