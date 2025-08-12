9b97a6251842adde35a20e37234c1a824f7ccce4
Robin Blondin <robin.blondin@protonmail.com>
2025-08-12 21:34:54 +0200

    Setup project

---
+++ src/main/kotlin/com/example/auroratracker/AuroraTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.auroratracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuroraTrackerApplication
+
+fun main(args: Array<String>) {
+      runApplication<AuroraTrackerApplication>(*args)
+}
