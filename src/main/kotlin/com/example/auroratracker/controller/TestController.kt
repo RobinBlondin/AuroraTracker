8a3d20e0a2b963d45e78679fa12badaf4b514e22
Robin Blondin <robin.blondin@protonmail.com>
2025-08-14 00:17:08 +0200

    Set up a test controller to test api call logic and mapping

---
+++ src/main/kotlin/com/example/auroratracker/controller/TestController.kt
@@ -0,0 +1,19 @@
+package com.example.auroratracker.controller
+
+import com.example.auroratracker.dto.UserDto
+import com.example.auroratracker.dto.WeatherResponseDto
+import com.example.auroratracker.service.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController(
+      val userService: UserService
+) {
+      @GetMapping("/test-weather")
+      fun testWeather(@RequestBody userDto: UserDto): Boolean {
+            return userService.isAfterSunsetAndClearSky(userDto)
+      }
+}
