7478a7f725fc464d4e0d52e5479ec80172ed365b
Robin Blondin <robin.blondin@protonmail.com>
2025-08-13 14:42:30 +0200

    Added Entity annotation and set table name because of SQL keyword issues

--- src/main/kotlin/com/example/auroratracker/entity/User.kt
+++ src/main/kotlin/com/example/auroratracker/entity/User.kt
@@ -1,8 +1,12 @@
 package com.example.auroratracker.entity
 
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
 import java.time.LocalDateTime
 
-class User(
+@Entity
+@Table(name = "users")
+class User (
       val name: String? = null,
       val email: String? = null,
       val phoneNumber: String? = null,
