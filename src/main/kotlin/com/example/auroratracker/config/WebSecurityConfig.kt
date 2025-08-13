25982d2f6cf7167e65566eeb932d35fb29855afb
Robin Blondin <robin.blondin@protonmail.com>
2025-08-14 00:20:19 +0200

    setup basic securityconfig

---
+++ src/main/kotlin/com/example/auroratracker/config/WebSecurityConfig.kt
@@ -0,0 +1,24 @@
+package com.example.auroratracker.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig{
+
+      @Bean
+      fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
+            http
+                  .cors { cors -> cors.disable() }
+                  .csrf { csrf -> csrf.disable() }
+                  .authorizeHttpRequests { auth ->
+                        auth
+                              .anyRequest().permitAll()
+                  }
+
+                  .build()
+}
