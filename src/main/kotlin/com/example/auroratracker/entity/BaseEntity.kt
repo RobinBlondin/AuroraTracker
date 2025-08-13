abc39acde6c3770935c24e2561700d19f84a0125
Robin Blondin <robin.blondin@protonmail.com>
2025-08-13 14:42:02 +0200

    Annotated the primary key

--- src/main/kotlin/com/example/auroratracker/entity/BaseEntity.kt
+++ src/main/kotlin/com/example/auroratracker/entity/BaseEntity.kt
@@ -1,16 +1,21 @@
 package com.example.auroratracker.entity
 
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
 import jakarta.persistence.PrePersist
 import jakarta.persistence.PreUpdate
 import java.time.LocalDateTime
 
 @MappedSuperclass
-class BaseEntity {
-      var id: Long? = null
-      var createdAt: LocalDateTime? = null
+abstract class BaseEntity (
+      @Id
+      @GeneratedValue(strategy = GenerationType.AUTO)
+      var id: Long? = null,
+      var createdAt: LocalDateTime? = null,
       var updatedAt: LocalDateTime? = null
-
+) {
       @PrePersist
       fun prePersist() {
             val now = LocalDateTime.now()
