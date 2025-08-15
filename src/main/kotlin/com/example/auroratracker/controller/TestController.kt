7bf5e05a0de6eef411acdfcdc2e0a48641d3d6bd
Robin Blondin <robin.blondin@protonmail.com>
2025-08-15 16:30:57 +0200

    Added new test endpoint

--- src/main/kotlin/com/example/auroratracker/controller/TestController.kt
+++ src/main/kotlin/com/example/auroratracker/controller/TestController.kt
@@ -1,19 +1,25 @@
 package com.example.auroratracker.controller
 
 import com.example.auroratracker.dto.UserDto
-import com.example.auroratracker.dto.WeatherResponseDto
+import com.example.auroratracker.service.TrackingService
 import com.example.auroratracker.service.UserService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestController(
-      val userService: UserService
+      val userService: UserService,
+      val trackingService: TrackingService
 ) {
       @GetMapping("/test-weather")
       fun testWeather(@RequestBody userDto: UserDto): Boolean {
             return userService.isAfterSunsetAndClearSky(userDto)
+      }
+
+      @GetMapping("/test-tracking")
+      fun testTracking() {
+            val kp = trackingService.getKpIndex()
+            println(kp)
       }
 }
