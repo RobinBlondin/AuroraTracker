79ccaf6cc5f36e9250d9e3f87c17b40ee8c8fd01
Robin Blondin <robin.blondin@protonmail.com>
2025-08-21 15:51:34 +0200

    Added scheduling and loading of env variables

--- src/main/kotlin/com/example/auroratracker/AuroraTrackerApplication.kt
+++ src/main/kotlin/com/example/auroratracker/AuroraTrackerApplication.kt
@@ -1,11 +1,19 @@
 package com.example.auroratracker
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class AuroraTrackerApplication
 
 fun main(args: Array<String>) {
+      val dotenv = Dotenv.configure().ignoreIfMissing().load()
+
+      dotenv.entries().forEach {
+            System.setProperty(it.key, it.value)
+      }
       runApplication<AuroraTrackerApplication>(*args)
 }
