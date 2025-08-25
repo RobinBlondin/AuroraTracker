5a00c2484a267221da919b40cb468d4c9cf32329
Robin Blondin <robin.blondin@protonmail.com>
2025-08-25 19:02:37 +0200

    Injected custom client and added function with error handling for api calls and parsing of responses

--- src/main/kotlin/com/example/auroratracker/service/JsonService.kt
+++ src/main/kotlin/com/example/auroratracker/service/JsonService.kt
@@ -1,23 +1,40 @@
 package com.example.auroratracker.service
 
+import com.example.auroratracker.config.CustomOkHttpClient
 import kotlinx.serialization.json.Json
-import okhttp3.OkHttpClient
 import okhttp3.Request
+import okio.IOException
 import org.springframework.stereotype.Service
 
 @Service
-class JsonService {
+class JsonService(
+      val client: CustomOkHttpClient
+) {
       val jsonParser: Json = Json { ignoreUnknownKeys = true }
 
       fun fetch(url: String): String {
-            val client = OkHttpClient()
             val request = Request.Builder().url(url).build()
             client.newCall(request).execute().use { response ->
+                  if (!response.isSuccessful) throw IOException("Unexpected code ${response.code} from $url")
                   return response.body.string()
             }
       }
 
       final inline fun <reified T> parse(json: String): T {
             return jsonParser.decodeFromString(json)
+      }
+
+      final inline fun <reified T> fetchAndParse(url: String): Result<T> {
+            try {
+                  val response = fetch(url)
+                  val result = parse<T>(response)
+                  return Result.success(result)
+            } catch (e: IOException) {
+                  println(e.message)
+                  return Result.failure(e)
+            } catch (e: Exception) {
+                  println("Failed to parse response from $url: ${e.message}")
+                  return Result.failure(e)
+            }
       }
 }
