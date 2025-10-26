b7ce581879d1411a1b3f28f7baf62abd13c038f0
Robin Blondin <robin.blondin@protonmail.com>
2025-10-26 12:10:01 +0100

    Added logic for setting userId and fetching user from database.

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    let userId = localStorage.getItem("userId")
+    if (!userId) {
+        userId = crypto.randomUUID()
+        localStorage.setItem("userId", userId)
+    }
 
+    const response = await fetch("/api/subscriptions/" + userId)
+        .then(data => {
+
+        })
+
+
+})
+
+const setPinOnMap = (lon, lat) => {
+    L.marker([lat, lon]).addTo(map);
+}
+
+const updateLastPosition = (lon, lat) => {
+
+}
