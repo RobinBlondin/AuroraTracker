eeb60a87682f3f56ab2c55635de237e3f58cb1dc
Robin Blondin <robin.blondin@protonmail.com>
2025-10-27 19:27:26 +0100

    Updated web security config with cors settings

--- src/main/kotlin/com/example/auroratracker/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/example/auroratracker/config/WebSecurityConfig.kt
@@ -5,20 +5,47 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
-class WebSecurityConfig{
+class WebSecurityConfig {
 
       @Bean
-      fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
+      fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
             http
-                  .cors { cors -> cors.disable() }
-                  .csrf { csrf -> csrf.disable() }
+                  .cors { }
+                  .csrf { it.disable() }
                   .authorizeHttpRequests { auth ->
                         auth
-                              .anyRequest().permitAll()
+                              .requestMatchers(
+                                    "/",
+                                    "/push",
+                                    "/api/subscriptions/subscribe",
+                                    "/api/subscriptions/unsubscribe",
+                                    "/css/**",
+                                    "/manifest.json",
+                                    "/sw,js",
+                                    "/script/**",
+                                    "/images/**"
+                              ).permitAll()
+                              .anyRequest().authenticated()
                   }
+                  .httpBasic {} // enkelt skydd för allt annat, kan bytas senare
+            return http.build()
+      }
 
-                  .build()
+      @Bean
+      fun corsConfigurationSource(): CorsConfigurationSource {
+            val config = CorsConfiguration()
+            config.allowedOrigins = listOf("https://www.auroratracker.se")
+            config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+            config.allowedHeaders = listOf("*")
+
+            val source = UrlBasedCorsConfigurationSource()
+            source.registerCorsConfiguration("/**", config)
+            return source
+      }
 }
