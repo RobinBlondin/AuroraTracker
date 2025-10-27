5d30285b39e6fd7280ed29a4a2e4c252a2ca558a
Robin Blondin <robin.blondin@protonmail.com>
2025-10-27 13:56:44 +0100

    Broke up and cleaned code.

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -3,152 +3,159 @@
 const map = L.map("map").setView([51.505, -0.09], 13);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 20,
-  attribution: "OpenStreetMap",
+    maxZoom: 20,
+    attribution: "OpenStreetMap",
 }).addTo(map);
 
 let currentMarker = null;
 
 function placeMarker(lat, lon) {
-  if (currentMarker) {
-    map.removeLayer(currentMarker);
-  }
-  currentMarker = L.marker([lat, lon]).addTo(map);
-  map.setView([lat, lon]);
+    if (currentMarker) {
+        map.removeLayer(currentMarker);
+    }
+    currentMarker = L.marker([lat, lon]).addTo(map);
+    map.setView([lat, lon]);
 }
 
 const unSubscribeOnLocation = async () => {
-  let userId = localStorage.getItem("userId");
-  const response = await fetch(
-    "http://localhost:8080/api/subscriptions/unsubscribe/" + userId,
-    {
-      method: "DELETE",
+    let userId = localStorage.getItem("userId");
+    await unsubscribeServiceWorker()
+
+    const response = await fetch(
+        "http://localhost:8080/api/subscriptions/unsubscribe/" + userId,
+        {
+            method: "DELETE",
+        }
+    );
+
+    if (response.ok) {
+        toggleDisplayMap("none")
     }
-  );
-
-  if (response.ok) {
-    const deletedLocation = await response.json();
-    map.remove();
-
-    const removed = document.getElementById("map");
-    removed.style.backgroundImage = "url('images/startpageformap.png')";
-    removed.style.backgroundPosition = "center";
-    removed.style.backgroundSize = "cover";
-    removed.style.backgroundRepeat = "no-repeat";
-  }
 };
 
 const toggleDisplayMap = (style) => {
-  const mapElement = document.getElementById("map");
-  mapElement.style.display = style;
+    const mapElement = document.getElementById("map");
+    mapElement.style.display = style;
 };
 
 const updateLocation = async () => {
-  navigator.geolocation.getCurrentPosition(
-    async (pos) => {
-      const lat = pos.coords.latitude;
-      const lon = pos.coords.longitude;
-      placeMarker(lat, lon);
-      const subscription = await subscribe(lat, lon);
-      await saveSubscription(subscription);
-      toggleDisplayMap("block");
-    },
-    (err) => {
-      console.error(err);
-    }
-  );
+    navigator.geolocation.getCurrentPosition(
+        async (pos) => {
+            const lat = pos.coords.latitude;
+            const lon = pos.coords.longitude;
+            placeMarker(lat, lon);
+            const subscription = await subscribe(lat, lon);
+            await saveSubscription(subscription);
+            toggleDisplayMap("block");
+        },
+        (err) => {
+            console.error(err);
+        }
+    );
 };
 
 /* =====  Helper functions ===== */
 
 const UI = {
-  setText(selector, text) {
-    document.querySelector(selector).textContent = text;
-  },
-  formatDate(dateString) {
-    const date = new Date(dateString);
-    return date.toISOString().slice(0, 16).replace("T", " ");
-  },
+    setText(selector, text) {
+        document.querySelector(selector).textContent = text;
+    },
+    formatDate(dateString) {
+        const date = new Date(dateString);
+        return date.toISOString().slice(0, 16).replace("T", " ");
+    },
 };
 
 function urlBase64ToUint8Array(base64String) {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
-  const rawData = atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
+    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+    const rawData = atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
 
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
 }
 
 /* ===== Push notification logic ===== */
 
 const subscribe = async (lat, lon) => {
-  let sw = await navigator.serviceWorker.ready;
-  let key = urlBase64ToUint8Array(PUBLIC_KEY);
+    const registration = await unsubscribeServiceWorker()
 
-  let push = (
-    await sw.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: key,
-    })
-  ).toJSON();
+    const push = (await registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(PUBLIC_KEY)
+    })).toJSON()
 
-  return {
-    endPoint: push.endpoint,
-    auth: push.keys.auth,
-    p256dh: push.keys.p256dh,
-    userId: localStorage.getItem("userId"),
-    lat: lat,
-    lon: lon,
-  };
+    return {
+        endPoint: push.endpoint,
+        auth: push.keys.auth,
+        p256dh: push.keys.p256dh,
+        userId: localStorage.getItem("userId"),
+        lat: lat,
+        lon: lon,
+    };
 };
 
 async function saveSubscription(data) {
-  const res = await fetch("/api/subscriptions/subscribe", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
+    const res = await fetch("/api/subscriptions/subscribe", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data),
+    });
 
-  if (!res.ok) throw new Error("Request failed");
-  return res.json();
+    if (!res.ok) throw new Error("Request failed");
+    return res.json();
+}
+
+async function unsubscribeServiceWorker() {
+    const registration = await navigator.serviceWorker.ready;
+    const existing = await registration.pushManager.getSubscription();
+    if (existing) {
+        await existing.unsubscribe();
+        console.log("Old subscription removed");
+    }
+
+    return registration
 }
 
 /* ===== UI functions ===== */
 
 const fetchUserDataAndUpdateElements = async (userId) => {
-  const response = await fetch("/api/subscriptions/" + userId);
-  if (response.status === 200) {
-    const user = await response.json();
-    placeMarker(user.lat, user.lon);
+    const response = await fetch("/api/subscriptions/" + userId);
+    if (response.status === 200) {
+        const user = await response.json();
+        placeMarker(user.lat, user.lon);
 
-    if (user.lastNotificationTime) {
-      UI.setText(
-        ".notification-timestamp",
-        UI.formatDate(user.lastNotificationTime)
-      );
+        if (user.lastNotificationTime) {
+            UI.setText(
+                ".notification-timestamp",
+                UI.formatDate(user.lastNotificationTime)
+            );
+        }
+
+        const positionString = `Latitude: ${user.lat.toFixed(
+            4
+        )}, Longitude: ${user.lon.toFixed(4)}`;
+        UI.setText(".position-data", positionString);
+    } else {
+        toggleDisplayMap("none");
     }
-
-    const positionString = `Latitude: ${user.lat.toFixed(
-      4
-    )}, Longitude: ${user.lon.toFixed(4)}`;
-    UI.setText(".position-data", positionString);
-  } else {
-    toggleDisplayMap("none");
-  }
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
-  let userId = localStorage.getItem("userId");
-  if (!userId) {
-    userId = crypto.randomUUID();
-    localStorage.setItem("userId", userId);
-  }
+    let userId = localStorage.getItem("userId");
+    if (!userId) {
+        userId = crypto.randomUUID();
+        localStorage.setItem("userId", userId);
+    }
 
-  await fetchUserDataAndUpdateElements(userId);
-  await navigator.serviceWorker.register("/sw.js");
-  console.log("SW registered");
+    console.log("PUBLIC_KEY", PUBLIC_KEY, PUBLIC_KEY.length);
+
+    await navigator.serviceWorker.register("/sw.js");
+    console.log("SW registered");
+
+    await navigator.serviceWorker.ready;
+    await fetchUserDataAndUpdateElements(userId);
 });
