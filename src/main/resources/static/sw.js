d1ffbd1e5e885affe491db3eaa0bf89b4d6c3f8c
Robin Blondin <robin.blondin@protonmail.com>
2025-10-27 13:55:18 +0100

    Updated service worker

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -1,9 +1,27 @@
-self.addEventListener('install', () => {
+self.addEventListener("install", (event) => {
+    console.log("[Service Worker] Installed");
     self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(clients.claim());
+self.addEventListener("activate", (event) => {
+    console.log("[Service Worker] Activated");
+    return self.clients.claim();
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("push", (event) => {
+    const data = event.data ? event.data.json() : {};
+    const title = data.title || "Aurora Alert";
+    const options = {
+        body: data.body || "New aurora activity detected!",
+        icon: "/images/icon-192.png",
+        badge: "/images/icon-192.png",
+        data: data.url || "/"
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener("notificationclick", (event) => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow(event.notification.data));
+});
