cfd6407d2f7323ca3476369746eb0bc0f91ffc43
Robin Blondin <robin.blondin@protonmail.com>
2025-10-28 11:44:21 +0100

    Added firebase initialization in service worker

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -24,3 +24,27 @@ self.addEventListener("notificationclick", (event) => {
     event.notification.close();
     event.waitUntil(clients.openWindow(event.notification.data));
 });
+
+/* For Chrome users via FCM */
+
+    importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
+    importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+    console.log("FCM background message:", payload);
+    const { title, body } = payload.notification;
+    self.registration.showNotification(title, { body });
+});
+
