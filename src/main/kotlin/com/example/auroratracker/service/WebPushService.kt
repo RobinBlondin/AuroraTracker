b6bafbd46484c0ac3bd4f921114b8f3c806eca44
Robin Blondin <robin.blondin@protonmail.com>
2025-10-28 11:45:30 +0100

    Changed name of service

--- src/main/kotlin/com/example/auroratracker/service/WebPushService.kt
+++ src/main/kotlin/com/example/auroratracker/service/WebPushService.kt
@@ -1,15 +1,21 @@
 package com.example.auroratracker.service
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import io.github.cdimascio.dotenv.Dotenv
 import nl.martijndwars.webpush.Notification
 import nl.martijndwars.webpush.PushService
 import nl.martijndwars.webpush.Subscription
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.springframework.stereotype.Service
+import java.io.ByteArrayInputStream
+import java.nio.charset.StandardCharsets
 import java.security.Security
 
+
 @Service
-class PushNotificationService {
+class WebPushService {
       private val dotenv = Dotenv.configure().ignoreIfMissing().load()
       init {
             Security.addProvider(BouncyCastleProvider())
@@ -18,8 +24,6 @@ class PushNotificationService {
       private val publicKey = dotenv["VAPID_PUBLIC_KEY"] ?: ""
       private val privateKey = dotenv["VAPID_PRIVATE_KEY"] ?: ""
       private val subject = dotenv["VAPID_SUBJECT"] ?: "mailto:robin.blondin@gmail.com"
-
-
       private val pushService = PushService(publicKey, privateKey, subject)
 
       fun sendNotification(endpoint: String?, p256dh: String?, auth: String?, message: String) {
