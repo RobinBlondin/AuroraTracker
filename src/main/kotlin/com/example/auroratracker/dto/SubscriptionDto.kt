10a1f72793f1e413d96b093ae7a0c7b1c3559309
Robin Blondin <robin.blondin@protonmail.com>
2025-10-28 11:44:40 +0100

    Added firebase token to entity and dto

--- src/main/kotlin/com/example/auroratracker/dto/SubscriptionDto.kt
+++ src/main/kotlin/com/example/auroratracker/dto/SubscriptionDto.kt
@@ -5,12 +5,13 @@ import java.time.ZonedDateTime
 
 class SubscriptionDto(
       var id: Long? = null,
-      var endPoint: String? = null,
+      var endpoint: String? = null,
       var p256dh: String? = null,
       var auth: String? = null,
       var userId: String? = null,
       var lon: Double? = null,
       var lat: Double? = null,
+      var firebaseToken: String? = null,
       var lastNotificationTime: ZonedDateTime? = null,
       var updatedAt: LocalDateTime? = null,
 )
