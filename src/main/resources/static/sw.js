4f8c287becc8cab1826e6ec6f8e80930c28f7c68
Robin Blondin <robin.blondin@protonmail.com>
2025-10-30 15:35:11 +0100

    removed logs

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -1,10 +1,8 @@
 self.addEventListener("install", (event) => {
-    console.log("[Service Worker] Installed");
     self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-    console.log("[Service Worker] Activated");
     return self.clients.claim();
 });
 
