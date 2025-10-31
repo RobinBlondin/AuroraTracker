b871ed49d583c1526537de60d37f330b9326de7a
Robin Blondin <robin.blondin@protonmail.com>
2025-10-31 22:59:55 +0100

    Sending statistics to frontend on load

--- src/main/kotlin/com/example/auroratracker/controller/IndexController.kt
+++ src/main/kotlin/com/example/auroratracker/controller/IndexController.kt
@@ -1,12 +1,26 @@
 package com.example.auroratracker.controller
 
+import com.example.auroratracker.service.NotificationService
+import com.example.auroratracker.service.TrackingService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class IndexController {
+class IndexController(
+      private val notificationService: NotificationService,
+      private val trackingService: TrackingService,
+) {
       @GetMapping("/")
-      fun index(): String {
+      fun index(model: Model): String {
+            val today = notificationService.countToday()
+            val total = notificationService.countTotal()
+            val kp = trackingService.getKpIndex()
+
+            model.addAttribute("kp", kp)
+            model.addAttribute("total", total)
+            model.addAttribute("today", today)
+
             return "index"
       }
 }
