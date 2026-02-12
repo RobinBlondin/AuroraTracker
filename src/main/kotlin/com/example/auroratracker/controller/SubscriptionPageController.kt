9fbe86918f003eedb99374b8c558b796dca4b6ec
Robin Blondin <robin.blondin@protonmail.com>
2026-02-12 17:09:03 +0100

    Created controller for signup page

---
+++ src/main/kotlin/com/example/auroratracker/controller/SubscriptionPageController.kt
@@ -0,0 +1,14 @@
+package com.example.auroratracker.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class SubscriptionPageController {
+
+    @GetMapping("/signup")
+    fun getSignupPage(): String {
+        return "signup"  // Thymeleaf template or React view
+    }
+
+}
